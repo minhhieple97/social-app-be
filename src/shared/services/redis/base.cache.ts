a5bd4a1d81ce317c76596201bc2606a81fe5fefc
hieple <hieplevuc@gmail.com>
2022-11-24 08:46:06 +0700

    add base cache class

--- src/shared/services/redis/base.cache.ts
+++ src/shared/services/redis/base.cache.ts
@@ -1,7 +1,9 @@
 import { createClient } from 'redis';
-
+import { config } from '@root/config';
+const Logger = config.createLogger('cache');
 const client = createClient();
 export type RedisClient = ReturnType<typeof createClient>;
 export abstract class BaseCache {
   client: RedisClient;
+  log: Logger;
 }
